dd58d1ef1c0ac4b1c56ccb7b3bf92bb04ebb9a11
s2322087 <s2322087@stu.musashino-u.ac.jp>
2024-05-22 01:12:21 +0900

    logの変更、フロントの変更

--- mygoproject/main.go
+++ mygoproject/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 type PageData struct {
@@ -13,7 +15,7 @@ type PageData struct {
 }
 
 // テンプレートのキャッシュ
-var templates = template.Must(template.ParseFiles("template.html"))
+var templates = template.Must(template.ParseFiles(filepath.Join("templates", "template.html")))
 
 // ハンドラ関数
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +29,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// ログミドルウェア
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf(
+			"%s %s %s %s %d %s\n",
+			r.RemoteAddr,
+			r.Method,
+			r.URL.Path,
+			r.Proto,
+			http.StatusOK,
+			time.Since(start),
+		)
+	})
+}
+
 func main() {
 	// ログファイルの設定
 	logFile, err := os.OpenFile("server.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -36,16 +55,19 @@ func main() {
 	log.SetOutput(logFile)
 
 	// 静的ファイルのサービング
-	staticDir := "static"
+	staticDir := filepath.Join("static")
 	fs := http.FileServer(http.Dir(staticDir))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// ルートハンドラの設定
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", handler)
+
+	// ログミドルウェアを使用
+	loggedRouter := loggingMiddleware(http.DefaultServeMux)
 
 	// サーバーの起動
 	log.Println("Starting server on :8080")
-	err = http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", loggedRouter)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
